d1ad3a12d079e3b9a4437e79757fab96f272cc73
Vic D'Elfant <vicdelfant@midd.ag>
2016-02-03 13:47:26 +0100

    Contact value object toegevoegd

---
+++ src/Values/Customers/Contact.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace JuriBlox\Sdk\Values\Customers;
+
+use JuriBlox\Sdk\Assertion;
+
+class Contact
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * CustomerContact constructor
+     *
+     * @param $name
+     * @param $email
+     */
+    public function __construct($name, $email)
+    {
+        Assertion::email($email);
+
+        $this->name = $name;
+        $this->email = strtolower($email);
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
