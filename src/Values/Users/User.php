b4b73c9ec609dcfd52352c87d0c69a454624372b
Vic D'Elfant <vicdelfant@midd.ag>
2016-02-10 14:37:50 +0100

    User value object

---
+++ src/Values/Users/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace JuriBlox\Sdk\Values\Users;
+
+class User
+{
+    /**
+     * @var UserId
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Create a User value object from raw text values
+     *
+     * @param   int     $id
+     * @param   string  $name
+     *
+     * @return  User
+     */
+    public static function fromText($id, $name)
+    {
+        return new static(new UserId($id), $name);
+    }
+
+    /**
+     * @param $id
+     * @param $name
+     */
+    public function __construct(UserId $id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->getId();
+    }
+
+    /**
+     * @return UserId
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
