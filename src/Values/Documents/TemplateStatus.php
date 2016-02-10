817e6ebc935996573c57596303106518f7c55186
Vic D'Elfant <vicdelfant@midd.ag>
2016-02-10 14:37:40 +0100

    TemplateStatus value object

---
+++ src/Values/Documents/TemplateStatus.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace JuriBlox\Sdk\Values\Documents;
+
+use JuriBlox\Sdk\Assertion;
+
+class TemplateStatus
+{
+    /**
+     * Template has been published
+     */
+    const STATUS_PUBLISHED = 1;
+
+    /**
+     * Available statuses
+     *
+     * @var array
+     */
+    public static $statuses = [
+        self::STATUS_PUBLISHED => 'Published'
+    ];
+
+    /**
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @param $code
+     * @param $name
+     */
+    public function __construct($code, $name)
+    {
+        Assertion::keyExists(static::$statuses, $code);
+
+        $this->code = $code;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->getCode();
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
