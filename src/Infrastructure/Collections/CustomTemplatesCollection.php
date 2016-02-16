292a478c9952dfe401f69c2183fee3ad342347d5
Vic D'Elfant <vicdelfant@midd.ag>
2016-02-16 14:43:51 +0100

    Template::$custom wel op true zetten

--- src/Infrastructure/Collections/CustomTemplatesCollection.php
+++ src/Infrastructure/Collections/CustomTemplatesCollection.php
@@ -7,12 +7,21 @@ use JuriBlox\Sdk\Infrastructure\Drivers\DriverInterface;
 class CustomTemplatesCollection extends TemplatesCollection
 {
     /**
-     * @param DriverInterface $driver
-     *
-     * @return TemplatesCollection
+     * {@inheritdoc}
      */
     public static function fromDriver(DriverInterface $driver)
     {
         return static::fromDriverWithSettings($driver, 'templates/custom', 'templates');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createEntityFromData($dto)
+    {
+        $template = parent::createEntityFromData($dto);
+        $template->setCustom(true);
+
+        return $template;
     }
 }
