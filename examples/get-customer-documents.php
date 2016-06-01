caba4e574598eb7838e5108bd64c62ea4350a32b
Vic D'Elfant <vic@wijzijnmiddag.nl>
2016-06-01 12:31:14 +0200

    Gewijzigde CustomerReference

--- examples/get-customer-documents.php
+++ examples/get-customer-documents.php
@@ -7,7 +7,7 @@ require __DIR__  . '/bootstrap.php';
 $application = new Application();
 $client = $application->getClient();
 
-foreach ($client->documents()->findByCustomer(new CustomerReference('add7e40d-ca6f-11e5-a679-56166038df7d')) as $document)
+foreach ($client->documents()->findByCustomer(new CustomerReference('b8b88723-ca6f-11e5-a679-56166038df7d')) as $document)
 {
     printTable([
         'ID'           => $document->getId(),
