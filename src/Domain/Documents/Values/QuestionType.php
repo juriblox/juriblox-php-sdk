05fe334a8011f5ed62022e7497b41b7b75bc42b6
Vic D'Elfant <vic@wijzijnmiddag.nl>
2016-11-08 14:39:49 +0100

    Extra question types toegevoegd

--- src/Domain/Documents/Values/QuestionType.php
+++ src/Domain/Documents/Values/QuestionType.php
@@ -7,19 +7,64 @@ use JuriBlox\Sdk\Validation\Assertion;
 class QuestionType
 {
     /**
-     * Radio button.
-     */
-    const TYPE_RADIO = 'radio';
-
-    /**
-     * Short text input field.
+     *
      */
     const TYPE_SHORT_TEXT = 'short_text';
 
     /**
-     * Boolean (yes/no) input field.
+     *
      */
-    const TYPE_BOOLEAN = 'yes_no';
+    const TYPE_LONG_TEXT = 'long_text';
+
+    /**
+     *
+     */
+    const TYPE_COC = 'kvk';
+
+    /**
+     *
+     */
+    const TYPE_PRICE = 'price';
+
+    /**
+     *
+     */
+    const TYPE_NUMERIC = 'numeric';
+
+    /**
+     *
+     */
+    const TYPE_DATE = 'date';
+
+    /**
+     *
+     */
+    const TYPE_YES_NO = 'yes_no';
+
+    /**
+     *
+     */
+    const TYPE_STATEMENT = 'statement';
+
+    /**
+     *
+     */
+    const TYPE_RADIO = 'radio';
+
+    /**
+     *
+     */
+    const TYPE_SELECTBOX = 'selectbox';
+
+    /**
+     *
+     */
+    const TYPE_CHECKBOX = 'checkbox';
+
+    /**
+     *
+     */
+    const TYPE_INFOBOX = 'infobox';
 
     /**
      * Available question types.
@@ -27,9 +72,18 @@ class QuestionType
      * @var array
      */
     public static $types = [
-        self::TYPE_BOOLEAN      => 'Boolean (yes/no)',
-        self::TYPE_RADIO        => 'Radio button',
         self::TYPE_SHORT_TEXT   => 'Short text input',
+        self::TYPE_LONG_TEXT    => 'Long text input',
+        self::TYPE_COC          => 'Chamber of Commerce registration number',
+        self::TYPE_PRICE        => 'Price',
+        self::TYPE_NUMERIC      => 'Numeric',
+        self::TYPE_DATE         => 'Date',
+        self::TYPE_YES_NO       => 'Boolean (yes/no)',
+        self::TYPE_STATEMENT    => 'Statement',
+        self::TYPE_RADIO        => 'Radio button',
+        self::TYPE_SELECTBOX    => 'Selectbox',
+        self::TYPE_CHECKBOX     => 'Checkbox',
+        self::TYPE_INFOBOX      => 'Information box'
     ];
 
     /**
