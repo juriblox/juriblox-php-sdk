4d639733612be73f84370930fb7f03ef25f53638
Vic D'Elfant <mail@vicdelfant.nl>
2021-07-14 09:50:07 +0200

    Merged missing preview endpoint (#4)

--- src/Infrastructure/Endpoints/TemplatesEndpoint.php
+++ src/Infrastructure/Endpoints/TemplatesEndpoint.php
@@ -2,10 +2,13 @@
 
 namespace JuriBlox\Sdk\Infrastructure\Endpoints;
 
+use JuriBlox\Sdk\Domain\Documents\Entities\Preview;
 use JuriBlox\Sdk\Domain\Documents\Entities\Template;
 use JuriBlox\Sdk\Domain\Documents\Values\TemplateId;
+use JuriBlox\Sdk\Domain\Documents\Entities\PreviewRequest;
 use JuriBlox\Sdk\Infrastructure\Collections\TemplatesCollection;
 use JuriBlox\Sdk\Infrastructure\Endpoints\Templates\QuestionnaireEndpoint;
+use JuriBlox\Sdk\Infrastructure\Transformers\Documents\PreviewTransformer;
 use JuriBlox\Sdk\Infrastructure\Transformers\Documents\TemplateTransformer;
 
 class TemplatesEndpoint extends AbstractEndpoint implements EndpointInterface
@@ -46,5 +49,20 @@ class TemplatesEndpoint extends AbstractEndpoint implements EndpointInterface
     public function questionnaire(TemplateId $id)
     {
         return QuestionnaireEndpoint::fromParentEndpoint($this, $id);
+    }
+    
+    /**
+     * @param PreviewRequest $request
+     *
+     * @return Preview
+     */
+    public function preview(PreviewRequest $request)
+    {
+        $result = $this->driver->post('templates/{id}/preview', [
+            'id'  => $request->getId()->getInteger(),
+            'css' => $request->isCss() ? 'true' : 'false'
+        ], ['answers' => $request->getAnswers()]);
+        
+        return PreviewTransformer::read($result);
     }
 }
